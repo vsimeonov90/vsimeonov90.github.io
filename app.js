0ea3d1632f9898e7f991786cdbfca9e4e4d94960
vsimeonov90 <vsimeonov@hotmail.com>
2024-12-07 04:27:36 +0200

    update

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-if (!lang) {
-  localStorage.setItem('lang','EN');
-}
+  if (!lang) {
+    setEN();
+  }
 
-headElement.innerHTML = `<br /><span style="font-size: 1.5rem;"><b>${headTitle}</b></span><br />`;
-headElement.innerHTML += `<span style="font-size: 1.2rem;"><b>${headSubTitle}</b></span><br />`;
-headElement.innerHTML += `<br /><a class="menuLink" href="javascript:setEN();">English</a> | <a class="menuLink" href="javascript:setBG();">Български</a><br /><br />`;
+  headElement.innerHTML = `<br /><span style="font-size: 1.5rem;"><b>${headTitle}</b></span><br />`;
+  headElement.innerHTML += `<span style="font-size: 1.2rem;"><b>${headSubTitle}</b></span><br />`;
+  headElement.innerHTML += `<br /><a class="menuLink" href="javascript:setEN();">English</a> | <a class="menuLink" href="javascript:setBG();">Български</a><br /><br />`;
 
-printMain();
+  printMain();
 
-menuBtn.addEventListener('mouseenter', menu);
-accrBtn.addEventListener('click', printAccr);
-accrBtn.addEventListener('mouseenter', hideMenu);
-bioLink.addEventListener('click', printBioHandler);
-skillsLink.addEventListener('click', printSkillsHandler);
-mainLink.addEventListener('click', printMainHandler);
+  menuBtn.addEventListener('mouseenter', menu);
+  accrBtn.addEventListener('click', printAccr);
+  accrBtn.addEventListener('mouseenter', hideMenu);
+  bioLink.addEventListener('click', printBioHandler);
+  skillsLink.addEventListener('click', printSkillsHandler);
+  mainLink.addEventListener('click', printMainHandler);
